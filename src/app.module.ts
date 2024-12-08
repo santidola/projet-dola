a0fb64814880659db0b241e6e3b7cfaf3c19e549
haroldCoder <haroldc2005@gmail.com>
2024-12-08 20:51:20 +0100

    create service for validation user with document

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { HelloController } from './hello/hello.controller';
+import { ApiSelecuModule } from './api-selecu/api-selecu.module';
 
 @Module({
-  imports: [tasksmodule, ProjectsModule, UsersModule, AuthModule],
+  imports: [tasksmodule, ProjectsModule, UsersModule, AuthModule, ApiSelecuModule],
   controllers: [HelloController],
 })
 export class AppModule {}
